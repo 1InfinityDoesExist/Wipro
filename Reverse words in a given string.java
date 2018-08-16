254376752108be1fef8890cf942ccb25da43ec7b
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-08-16 09:21:34 +0530

    Add files via upload

--- Reverse words in a given string.java
+++ Reverse words in a given string.java
@@ -1,36 +1,79 @@
-import java.io.*;
-import java.lang.*;
-import java.util.*;
+/*package whatever //do not write package name here */
 
-public class GFG
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
 {
-	public String reverseSentense(String str)
+	public String reverseString(String st)
 	{
-		char[] s = str.toCharArray();
+		char[] ch = st.toCharArray();
 		int start = 0;
-		int end = str.length()-1;
+		int end = st.length()-1;
 		while(start < end)
 		{
-			char temp = s[start];
-			s[start] = s[end];
-			s[end] = temp;
+			char temp = ch[start];
+			ch[start] = ch[end];
+			ch[end] = temp;
 			start++;
 			end--;
 		}
-		String sk = new String(s);
-		return sk;
+		return new String(ch);
 	}
-	public static void main(String[] args) throws  IOException
+	public String reverseWordOfSentense(String str)
+	{
+		Stack<String> st = new Stack<String>();
+		String s = "";
+		for(int iter = 0; iter < str.length(); iter++)
+		{
+			if(str.charAt(iter) == '.')
+			{
+				if(s.length() == 0)
+				{
+					continue;
+				}
+				else
+				{
+					//st.push(reverseString(s));
+					st.push(s);
+					s = "";
+				}
+			}
+			else
+			{
+				s = s + str.charAt(iter);
+				if(iter == str.length()-1)
+				{
+					st.push(s);
+				}
+			}
+		}
+		str = "";
+		while(!st.empty())
+		{
+			if(st.size() == 1)
+			{
+				str = str + st.peek();
+				return str;
+			}
+			str = str + st.peek();
+			str = str + ".";
+			st.pop();
+		}
+		return str;
+	}
+	public static void main (String[] args) throws IOException
 	{
 		GFG gfg = new GFG();
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 
 		int testCase = Integer.parseInt(br.readLine());
-		for(int iter = 1; iter <= testCase; iter++)
+		while(testCase-- > 0)
 		{
 			String str = br.readLine();
-			System.out.println(gfg.reverseSentense(str));
+			System.out.println(gfg.reverseWordOfSentense(str));
 		}
 	}
 }
